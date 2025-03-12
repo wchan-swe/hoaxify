c2772e0e323c70aec0cac2837af6f20876bfc3fd
Wayne Chan <wchan.it@outlook.com>
2025-03-12 07:03:20 -0500

    Add sending requests to backend

--- hoaxify-frontend/src/api/apiCalls.spec.js
+++ hoaxify-frontend/src/api/apiCalls.spec.js
@@ -1,15 +1,25 @@
 import axios from 'axios';
-import * as apicalls from './apiCalls';
+import * as apiCalls from './apiCalls';
 
 describe('apiCalls', () => {
   describe('signup', () => {
     it('calls /api/1.0/users', async () => {
       const mockSignup = jest.fn();
       axios.post = mockSignup;
-      apicalls.signup();
-
+      apiCalls.signup();
+      // get the first argument of the first call
       const path = mockSignup.mock.calls[0][0];
       expect(path).toBe('/api/1.0/users');
+    });
+  });
+
+  describe('login', () => {
+    it('calls /api/1.0/login', async () => {
+      const mockLogin = jest.fn();
+      axios.post = mockLogin;
+      apiCalls.login({ username: 'test-user', password: 'P4ssword' });
+      const path = mockLogin.mock.calls[0][0];
+      expect(path).toBe('/api/1.0/login');
     });
   });
 });
